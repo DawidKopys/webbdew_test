37b2910d690bd5a164c8f650d86814c9e72ece96
Dawid Kopys <dewke17@gmail.com>
2019-12-15 20:52:56 +0100

    Add second image, add button, ask user for his name and display it in the h1 element

--- test-site/scripts/main.js
+++ test-site/scripts/main.js
@@ -1,2 +1,35 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+let myImage = document.querySelector("img");
+
+myImage.onclick = function() {
+  let mySrc = myImage.getAttribute("src");
+  if (mySrc == "images/meme.png") {
+    myImage.setAttribute("src", "images/meme2.png");
+  } else {
+    myImage.setAttribute("src", "images/meme.png");
+  }
+}
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName || myName === null) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.textContent = "GIB ME15k, " + myName;
+  }
+}
+
+/* Initialization code */
+if (!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = "GIB ME 15k, "  + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
